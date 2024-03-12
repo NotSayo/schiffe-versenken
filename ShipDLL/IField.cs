884b2b239d514ab98c12952795ff664ad04f0e81
NotSayo <olafplazinski23@gmail.com>
2024-03-12 09:24:26 +0100

    added fields

--- ShipDLL/IField.cs
+++ ShipDLL/IField.cs
@@ -2,5 +2,7 @@
 
 public interface IField
 {
-    
+    public List<IShip> Ships { get; set; }
+    public int LeftHP { get; set; }
+    public int LeftShips { get; set; }
 }
--- ShipDLL/IPlayer.cs
+++ ShipDLL/IPlayer.cs
@@ -1,6 +1,10 @@
-﻿namespace ShipDLL;
+﻿using System.Runtime.CompilerServices;
+
+namespace ShipDLL;
 
 public interface IPlayer
 {
+    public int ID { get; set; }
     
+    public IField Field { get; set; }
 }
